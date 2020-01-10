3f1f1bc7da31ebf7981f5ca57ad7375ed78a4f13
sametemek <sametemek@windowslive.com>
2020-01-10 10:58:51 +0300

    sample unit tests with MSTest

---
+++ Unitest.Demo/Unitest.Demo/CalculatorService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unitest.Demo
+{
+    public class CalculatorService
+    {
+        public int Plus(int digit1, int digit2)
+        {
+            if (digit1 < 0 || digit2 < 0)
+            {
+                throw new DigitMustBePositive();
+            }
+
+            return digit1 + digit2;
+        }
+
+        public int Substract(int digit1, int digit2)
+        {
+            return digit1 - digit2;
+        }
+    }
+}
---
+++ Unitest.Demo/Unitest.Demo/Exceptions.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unitest.Demo
+{
+    public class DigitMustBePositive : System.Exception { }
+}
